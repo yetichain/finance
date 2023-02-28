7ef7352bb7059d3756ce9e70cfebba016b69f66f
Yetichain <118218938+yetichain@users.noreply.github.com>
2023-02-28 14:55:53 -0600

    Add files via upload

---
+++ public/images/farms/icons/sort-card-icon.tsx
@@ -0,0 +1,34 @@
+export const SortCardIcon = () => {
+  return (
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M5 2.75H9C9.69036 2.75 10.25 3.30964 10.25 4V9C10.25 9.69036 9.69036 10.25 9 10.25H5C4.30964 10.25 3.75 9.69036 3.75 9V4C3.75 3.30964 4.30964 2.75 5 2.75Z'
+        stroke='#D2D6E5'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M15 2.75H19C19.6904 2.75 20.25 3.30964 20.25 4V9C20.25 9.69036 19.6904 10.25 19 10.25H15C14.3096 10.25 13.75 9.69036 13.75 9V4C13.75 3.30964 14.3096 2.75 15 2.75Z'
+        stroke='#D2D6E5'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M15 13.75H19C19.6904 13.75 20.25 14.3096 20.25 15V20C20.25 20.6904 19.6904 21.25 19 21.25H15C14.3096 21.25 13.75 20.6904 13.75 20V15C13.75 14.3096 14.3096 13.75 15 13.75Z'
+        stroke='#D2D6E5'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M5 13.75H9C9.69036 13.75 10.25 14.3096 10.25 15V20C10.25 20.6904 9.69036 21.25 9 21.25H5C4.30964 21.25 3.75 20.6904 3.75 20V15C3.75 14.3096 4.30964 13.75 5 13.75Z'
+        stroke='#D2D6E5'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
---
+++ public/images/farms/icons/sort-table-icon.tsx
@@ -0,0 +1,48 @@
+export const SortTableIcon = () => {
+  return (
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M3 3.75H5C5.13807 3.75 5.25 3.86193 5.25 4V6C5.25 6.13807 5.13807 6.25 5 6.25H3C2.86193 6.25 2.75 6.13807 2.75 6V4C2.75 3.86193 2.86193 3.75 3 3.75Z'
+        stroke='#D2D6E5'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M3 10.75H5C5.13807 10.75 5.25 10.8619 5.25 11V13C5.25 13.1381 5.13807 13.25 5 13.25H3C2.86193 13.25 2.75 13.1381 2.75 13V11C2.75 10.8619 2.86193 10.75 3 10.75Z'
+        stroke='#D2D6E5'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M9 3.75H21C21.1381 3.75 21.25 3.86193 21.25 4V6C21.25 6.13807 21.1381 6.25 21 6.25H9C8.86193 6.25 8.75 6.13807 8.75 6V4C8.75 3.86193 8.86193 3.75 9 3.75Z'
+        stroke='#D2D6E5'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M9 10.75H21C21.1381 10.75 21.25 10.8619 21.25 11V13C21.25 13.1381 21.1381 13.25 21 13.25H9C8.86193 13.25 8.75 13.1381 8.75 13V11C8.75 10.8619 8.86193 10.75 9 10.75Z'
+        stroke='#D2D6E5'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M3 17.75H5C5.13807 17.75 5.25 17.8619 5.25 18V20C5.25 20.1381 5.13807 20.25 5 20.25H3C2.86193 20.25 2.75 20.1381 2.75 20V18C2.75 17.8619 2.86193 17.75 3 17.75Z'
+        stroke='#D2D6E5'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M9 17.75H21C21.1381 17.75 21.25 17.8619 21.25 18V20C21.25 20.1381 21.1381 20.25 21 20.25H9C8.86193 20.25 8.75 20.1381 8.75 20V18C8.75 17.8619 8.86193 17.75 9 17.75Z'
+        stroke='#D2D6E5'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
