997f6305918b53b9dff9adaf12dc36e6f5d44c65
Yetichain <118218938+yetichain@users.noreply.github.com>
2023-02-28 15:56:42 -0600

    Create .env.development.js

---
+++ .env.development.js
@@ -0,0 +1,31 @@
+/*
+Public PRC nodes can be taken from here:
+BSC: https://docs.binance.org/smart-chain/developer/rpc.html
+HECO: https://www.hecochain.com/developer-en.9d3e5b2.pdf
+MATIC: https://docs.matic.network/docs/develop/network-details/network/
+ETHEREUM: https://docs.linkpool.io/docs/public_rpc
+*/
+module.exports = {
+  APP_NODES_BSC: [
+    'https://data-seed-prebsc-1-s1.binance.org:8545',
+    'https://data-seed-prebsc-2-s1.binance.org:8545',
+    'https://data-seed-prebsc-1-s2.binance.org:8545',
+    'https://data-seed-prebsc-2-s2.binance.org:8545',
+    'https://data-seed-prebsc-1-s3.binance.org:8545',
+  ],
+  APP_NODES_HECO: ['https://http-testnet.hecochain.com'],
+  APP_NODES_ETHEREUM: ['https://rinkeby.infura.io/v3/d32640fa3a7f4a149068228024a399e4'],
+  APP_NODES_MATIC: ['https://rpc-mainnet.maticvigil.com/'],
+  APP_NFT_PUBLIC_SELLER_ADDRESS: '0xF92dC46c2F373480cbC7Dacb0A003C4a2c23ea78',
+  APP_NFT_COLLECTIBLE_ADDRESS: '0xD06E646C1bf3fA870875A375C58A6b5326b6bC49',
+  APP_NFT_ALIUM_ARCHIVEMENT_COLLECTIBLE: '0x1b1CD5E15C1d0E84cbffF8F7Df514e4f90031FdC',
+  APP_NFT_ALIUM_COLLECTIBLE_NFT: '0xD06E646C1bf3fA870875A375C58A6b5326b6bC49',
+  APP_NFT_ALIUM_CHASHBOX: '0x48652e59141290783C713fB3C6d066A317351C4B',
+  APP_NFT_ALIUM_TOKEN: '0x6f58aCfaEB1BfDC9c4959c43aDdE7a3b63BF019f',
+  APP_NFT_EXCHANGER_PRIVATE: '0x759Bb076a05a89915a2996AF02d936DE8500798f',
+  APP_NFT_EXCHANGER_STRATEGICAL_PRIVATE: '0x759Bb076a05a89915a2996AF02d936DE8500798f',
+  APP_NFT_EXCHANGER_PUBLIC: '0x759Bb076a05a89915a2996AF02d936DE8500798f',
+  APP_NFT_VESTING: '0x3983e76FD2208a7b092785246E8f83B5c4e04E99',
+  APP_NFT_VESTING_TOTAL_VIA_TRANSIVER: '0x3983e76FD2208a7b092785246E8f83B5c4e04E99',
+  APP_INTERCOM_APP_ID: null,
+}
